77ac92706f2e4018387405aea665f1641cd30e36
lois burg <burg_l@pc-burg-l.(none)>
2012-05-03 12:32:06 +0200

    Ajout des fichiers Block

---
+++ Block.cpp
@@ -0,0 +1,42 @@
+//
+// Block.cpp for bomberman in /home/burg_l//Work/tek2/cpp/Bomberman
+//
+// Made by lois burg
+// Login   <burg_l@epitech.net>
+//
+// Started on  Thu May  3 12:08:17 2012 lois burg
+// Last update Thu May  3 12:24:49 2012 lois burg
+//
+
+#include "Block.hh"
+
+using namespace	Bomberman;
+
+Block::Block()
+  : AObject("Block")
+{
+}
+
+// Block::Block(const Vector3d& pos, const Vector3d& rot, const Vector3d& sz)
+//   : AObject(pos, rot, sz,"Block")
+// {
+// }
+
+Block::~Block()
+{
+}
+
+void		Block::update(const gdl::GameClock& clock, const gdl::Input& keys)
+{
+  (void)clock;
+  (void)keys;
+}
+
+void		Block::draw(void)
+{
+}
+
+const std::string&	Block::type(void) const
+{
+  return (type_);
+}
---
+++ Block.hh
@@ -0,0 +1,31 @@
+//
+// Block.hh for bomberman in /home/burg_l//Work/tek2/cpp/Bomberman
+//
+// Made by lois burg
+// Login   <burg_l@epitech.net>
+//
+// Started on  Thu May  3 12:02:52 2012 lois burg
+// Last update Thu May  3 12:21:55 2012 lois burg
+//
+
+#ifndef		__BLOCK_HH__
+# define	__BLOCK_HH__
+
+# include	"AObject.hh"
+
+namespace	Bomberman
+{
+  class	Block : public AObject
+  {
+  public:
+    Block();
+    // Block(const Vector3d& pos, const Vector3d& rot, const Vector3d& sz);
+    virtual ~Block();
+
+  public:
+    virtual void		update(const gdl::GameClock& clock, const gdl::Input& keys);
+    virtual void		draw(void);
+    virtual const std::string&	type(void) const;
+  };
+}
+#endif /* !__BLOCK_HH__*/
