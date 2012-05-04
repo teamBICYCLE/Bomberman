89f094b29beba45ec23c87e0864c09e5cdc0cda0
geoffroy lafontaine <lafont_g@pc-lafont-g.(none)>
2012-05-04 18:25:55 +0200

    map generation.

--- Map.cpp
+++ Map.cpp
@@ -5,15 +5,25 @@
 // Login   <burg_l@epitech.net>
 //
 // Started on  Fri May  4 15:28:37 2012 lois burg
-// Last update Fri May  4 15:37:51 2012 lois burg
+// Last update Fri May  4 18:13:47 2012 geoffroy lafontaine
 //
 
+#include <iterator>
 #include "MapManager.hh"
 
 using namespace Bomberman;
 
-Map::Map(void)
+Map::Map(unsigned int width, unsigned int height, unsigned int nbPlayers)
 {
+  for (int y = 1; y < height - 1; y += 2)
+    {
+      for (int x = 1; x < width - 1; x += 2)
+	{
+	  terrain_.push_back(new Block(Vector3d(x,y,0), Vector3d(0,0,0), Vector3d(0,0,0)));
+	}
+    }
+  addPlayers(width, height, nbPlayers);
+  generateBricks(width, height, nbPlayers);
 }
 
 Map::Map(const std::string& fileName)
@@ -27,4 +37,44 @@ Map::~Map(void)
 const std::vector<AObject*>&	Map::getTerrain(void) const
 {
   return (terrain_);
+}
+
+void				Map::generateBricks(unsigned int width, unsigned int height,
+						    unsigned int nbPlayers)
+{
+  unsigned int				nbBricks = 0;
+  std::vector<AObject*>::iterator	it;
+
+  nbBricks = (width * height) - terrain_.size() - (3 * nbPlayers) - ((width > height ? width : height));
+  do {
+    unsigned int x = rand() % width;
+    unsigned int y = rand() % height;
+
+    if ((x % 2) && (width - 1) > (x + 1))
+      ++x;
+    if ((y % 2) && (height - 1) > (y + 1))
+      ++y;
+    for (it = terrain_.begin(); it != terrain_.end(); ++it)
+      {
+	if ((*it).getPos().x != x && (*it).getPos().y != y)
+	  {
+	    terrain_.push_back(new Brick(Vector3d(x,y,0), Vector3d(0,0,0), Vector3d(0,0,0)));
+	    --nbBricks;
+	    break;
+	  }
+      }   
+  }
+  while (nbBricks > 0);
+}
+
+void				Map::addPlayers(unsigned int width, unsigned int height,
+						unsigned int nbPlayers)
+{
+  terrain_.push_back(new Player(Vector3d(0,0,0), Vector3d(0,0,0), Vector3d(0,0,0)));
+  if (nbPlayers > 1)
+    terrain_.push_back(new Player(Vector3d(width - 1,height - 1,0), Vector3d(0,0,0), Vector3d(0,0,0)));
+  if (nbPlayers > 2)
+    terrain_.push_back(new Player(Vector3d(0,height - 1,0), Vector3d(0,0,0), Vector3d(0,0,0)));
+  if (nbPlayers > 3)
+    terrain_.push_back(new Player(Vector3d(width - 1,0,0), Vector3d(0,0,0), Vector3d(0,0,0)));
 }
--- Map.hh
+++ Map.hh
@@ -5,7 +5,7 @@
 // Login   <burg_l@epitech.net>
 //
 // Started on  Fri May  4 15:29:12 2012 lois burg
-// Last update Fri May  4 15:33:24 2012 lois burg
+// Last update Fri May  4 17:10:12 2012 geoffroy lafontaine
 //
 
 #ifndef		__MAPMANAGER_HH__
@@ -18,7 +18,7 @@ namespace	Bomberman
   class	Map
   {
   public:
-    Map(void);
+    Map(unsigned int width = 13, unsigned int height = 13, unsigned int nbPlayers);
     Map(const std::string& fileName);
     ~Map(void);
 
