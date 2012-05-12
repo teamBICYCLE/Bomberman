8f1d18303d142918af0c82a4ba09b74a85066a51
lois burg <burg_l@pc-burg-l.(none)>
2012-05-12 18:59:36 +0200

    Le joueur peut glisser le long des murs

--- PlayState.cpp
+++ PlayState.cpp
@@ -5,7 +5,7 @@
 // Login   <burg_l@epitech.net>
 //
 // Started on  Wed May  2 18:00:30 2012 lois burg
-// Last update Fri May 11 09:54:45 2012 romain sylvian
+// Last update Sat May 12 18:21:55 2012 lois burg
 //
 
 #include <unistd.h>
