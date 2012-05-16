aa9d812903dfffd502792f78367a9fbe6036e1b8
Loadex <carpen_t@epitech.eu>
2012-05-16 12:22:12 +0200

    ajout lua

---
+++ ia/luainc.hh
@@ -0,0 +1,21 @@
+//
+// luainc.hh for  in /home/carpen_t/projets/c++/iabomber
+//
+// Made by thibault carpentier
+// Login   <carpen_t@epitech.net>
+//
+// Started on  Fri May  4 14:30:50 2012 thibault carpentier
+// Last update Fri May  4 15:07:17 2012 thibault carpentier
+//
+
+#ifndef _LUAINC_H_
+# define _LUAINC_H_
+
+ extern "C"
+ {
+  # include <lua.h>
+  # include <lauxlib.h>
+  # include <lualib.h>
+ }
+
+#endif /*!_LUAINC_H_*/
