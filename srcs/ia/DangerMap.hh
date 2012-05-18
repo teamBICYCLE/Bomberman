963f03c2a661c00fac961ee742a85dbd8bb845d5
Loadex <carpen_t@epitech.eu>
2012-05-18 14:48:27 +0200

    ajout dangerMap

---
+++ srcs/ia/DangerMap.hh
@@ -0,0 +1,25 @@
+//
+// DangerMap.hh for  in /home/carpen_t/projets/c++/Bomberman
+//
+// Made by thibault carpentier
+// Login   <carpen_t@epitech.net>
+//
+// Started on  Fri May 18 14:30:13 2012 thibault carpentier
+// Last update Fri May 18 14:31:25 2012 thibault carpentier
+//
+
+#ifndef _DANGERMAP_H_
+# define _DANGERMAP_H_
+
+class DangerMap
+{
+private:
+  DangerMap(DangerMap const &);
+  DangerMap& operator=(DangerMap const &);
+
+public:
+  DangerMap();
+  ~DangerMap();
+};
+
+#endif /*!_DANGERMAP_H_*/
