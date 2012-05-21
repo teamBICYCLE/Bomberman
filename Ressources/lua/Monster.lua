3e714316673de90fa3bc31b7ab9136238c4bbde0
Loadex <carpen_t@epitech.eu>
2012-05-21 15:27:40 +0200

    ajout du deplacement pseudo intelligent des bots

--- Ressources/lua/Monster.lua
+++ Ressources/lua/Monster.lua
@@ -7,25 +7,25 @@
 function this.thinking(this, x, y)
    --setHook(MASKCOUNT, 4)
   --printDebug("Thinking Ia !")
-  print (x, y)
-  print (this:isCrossable(x, y - 1, MONSTER))
+  -- print (x, y)
+  -- print (this:isCrossable(x, y - 1, MONSTER))
   math.randomseed(os.time())
   a = math.random(1, 5)
   if a == 1 and this:isCrossable(x, y - 1, MONSTER) == 1
   then
-     printDebug("UP")
+     -- printDebug("UP")
      return UP
   elseif a == 2 and this:isCrossable(x + 1, y, MONSTER) == 1
   then
-     printDebug("RIGHT")
+     -- printDebug("RIGHT")
      return RIGHT
   elseif a == 3 and this:isCrossable(x, y + 1, MONSTER) == 1
   then
-     printDebug("DOWN")
+     -- printDebug("DOWN")
      return DOWN
   elseif a == 4 and this:isCrossable(x - 1, y, MONSTER) == 1
   then
-     printDebug("LEFT")
+     -- printDebug("LEFT")
      return LEFT
   end
 return NODIR
