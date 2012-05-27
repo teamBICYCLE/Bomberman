85c792772be88961b01025ed95fc7ac8df0dcded
Loadex <carpen_t@epitech.eu>
2012-05-27 09:44:56 +0200

    oubli d un fichier

---
+++ srcs/game/Danger.hh
@@ -0,0 +1,26 @@
+//
+// Danger.hh for  in /home/carpen_t/projets/c++/Bomberman
+//
+// Made by thibault carpentier
+// Login   <carpen_t@epitech.net>
+//
+// Started on  Sat May 26 15:55:31 2012 thibault carpentier
+// Last update Sat May 26 16:42:20 2012 thibault carpentier
+//
+
+#ifndef _DANGER_H_
+# define _DANGER_H_
+
+# define DANGER_MAX 10
+# define DANGER_MIN -10
+
+# define DANGER_BOMB (DANGER_MAX - (getTimeOut() * 3))
+# define DANGER_BLOCK 0
+# define DANGER_MONSTER 0
+# define DANGER_PLAYER DANGER_MIN
+# define DANGER_POWERUP (DANGER_MIN / 2)
+# define DANGER_MINE DANGER_MAX
+# define DANGER_EXPLOSION DANGER_MAX
+# define DANGER_FIREBLOCK (DANGER_MAX - (getTimeOut() *3))
+
+#endif /*!_DANGER_H_*/
