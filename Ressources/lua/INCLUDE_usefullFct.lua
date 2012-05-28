b74a6a424faefa7d03cf223eabdbe5c090cab884
Loadex <carpen_t@epitech.eu>
2012-05-28 16:26:47 +0200

    ia

--- Ressources/lua/INCLUDE_usefullFct.lua
+++ Ressources/lua/INCLUDE_usefullFct.lua
@@ -24,9 +24,9 @@ end
 function getZoneDanger(this, x, y)
    local posX = {x , x + 1 , x - 1 , x     , x     , x + 1 , x - 1 , x + 1 , x - 1}
    local posY = {y , y     , y     , y - 1 , y + 1 , y + 1 , y + 1 , y - 1 , y - 1}
-
    local res = 0
    local size = 0
+
    for j = 1, table.getn(posX) do
       local tmp = this:getDanger(posX[j], posY[j])
 --      print("danger : ", "x ", posX[j], " y ", posY[j], this:getDanger(posX[j], posY[j]))
@@ -56,16 +56,16 @@ function getLessDangerousDirection(this, x, y, type)
    local dir = {NODIR, RIGHT   , LEFT    , UP      , DOWN     }
    local posX = {x   , x + 1   , x - 1   , x       , x        }
    local posY = {y   , y       , y       , y - 1   , y + 1    }
-   local dirX = {x   , x + 1, x - 1, x       , x        }
-   local dirY = {y   , y       , y       , y - 1, y + 1 }
+   local dirX = {x   , x + 0.05, x - 0.05, x       , x        }
+   local dirY = {y   , y       , y       , y - 0.05, y + 0.05 }
 
---   print(posX[1], posX[2], posX[3])
+   --   print(posX[1], posX[2], posX[3])
    local res_dir = dir[1]
    local danger_res = getZoneDanger(this, posX[1], posY[1])
-   print("Danger local de ", danger_res )
+   --   print("Danger local de ", danger_res )
    for  i = 1, table.getn(dir) do
       local tmpDanger = getZoneDanger(this, posX[i], posY[i])
-    print("================= Testing : ", showdir(dir[i]) ," danger de :", tmpDanger, "et crossable == ", this:isCrossable(dirX[i], dirY[i], type))
+      --   print("================= Testing : ", showdir(dir[i]) ," danger de :", tmpDanger, "et crossable == ", this:isCrossable(dirX[i], dirY[i], type))
       if (danger_res > 0 and
       tmpDanger < danger_res
 	  and this:isCrossable(dirX[i], dirY[i], type) == 1)
@@ -75,7 +75,7 @@ function getLessDangerousDirection(this, x, y, type)
 	 --print(showdir(res_dir))
       end
    end
-    print("======", showdir(res_dir), "=======")
+   -- print("======", showdir(res_dir), "=======")
   -- print()
   -- print()
    return (res_dir)
