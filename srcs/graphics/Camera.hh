b30a5a76d119f8b4666f53abf3923a1ebcbe23dc
Romain Sylvian <romain.sylvian@epitech.eu>
2012-05-31 18:34:36 +0200

    suppresion de useless fichier

--- srcs/graphics/Camera.hh
+++ srcs/graphics/Camera.hh
@@ -28,16 +28,16 @@ public:
   void    drawRepere(void);
 
 private:
-// static const float fov = 70.0f;
-// static const float winxSize = 800.0f;
-// static const float winySize = 600.0f;
-// static const float zNear = 1.0f;
-// static const float zFar = 1000.0f;
-   const float fov = 70.0f;
-   const float winxSize = 1600.0f;
-   const float winySize = 900.0f;
-   const float zNear = -1000.0f;
-   const float zFar = 1000.0f;
+ static const float fov = 70.0f;
+ static const float winxSize = 800.0f;
+ static const float winySize = 600.0f;
+ static const float zNear = 1.0f;
+ static const float zFar = 1000.0f;
+   // const float fov = 70.0f;
+   // const float winxSize = 1600.0f;
+   // const float winySize = 900.0f;
+   // const float zNear = -1000.0f;
+   // const float zFar = 1000.0f;
 
 protected:
   Vector3d    position_;
